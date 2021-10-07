7df5443f3e74c792071b34883a33910f3aa5d627
wukefan <wdsqq135@126.com>
2021-10-07 21:40:18 +0800

    push test

--- git-test/src/main/java/com/wkf/ms/git/GitTest2.java
+++ git-test/src/main/java/com/wkf/ms/git/GitTest2.java
@@ -4,5 +4,6 @@ public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("master test1");
         System.out.println("hot-fix test2");
+        System.out.println("push test");
     }
 }
