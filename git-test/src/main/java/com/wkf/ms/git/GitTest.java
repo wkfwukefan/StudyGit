4decc68067beeee0f07fa273628ba7686c15206f
wukefan <wdsqq135@126.com>
2021-10-07 21:06:47 +0800

    master test commit

--- git-test/src/main/java/com/wkf/ms/git/GitTest.java
+++ git-test/src/main/java/com/wkf/ms/git/GitTest.java
@@ -6,6 +6,8 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hot-fix=====1");
+        System.out.println("hot-fix test");
         System.out.println("master test");
+        System.out.println("hot-fix2");
     }
 }
