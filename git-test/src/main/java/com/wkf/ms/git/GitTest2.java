cf2662666ace737b450016ed4c31696469fd44c3
wukefan <wdsqq135@126.com>
2021-10-07 21:13:05 +0800

    new hf test2

--- git-test/src/main/java/com/wkf/ms/git/GitTest2.java
+++ git-test/src/main/java/com/wkf/ms/git/GitTest2.java
@@ -3,5 +3,6 @@ package com.wkf.ms.git;
 public class GitTest2 {
     public static void main(String[] args) {
         System.out.println("hot-fix test1");
+        System.out.println("hot-fix test2");
     }
 }
